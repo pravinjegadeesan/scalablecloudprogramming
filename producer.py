74f49fff7d9b07660df0e1140f748530e639765a
demo <arunnatarajan278@gmail.com>
2026-07-16 15:01:02 +0100

    Initial project setup and add producer.py

---
+++ producer.py
@@ -0,0 +1,13 @@
+"""
+Spotify Charts Producer
+
+Reads Spotify public charts RSS feed
+and prepares data for streaming into AWS Kinesis.
+"""
+
+def main():
+    print("Spotify Producer Started")
+
+
+if __name__ == "__main__":
+    main()
